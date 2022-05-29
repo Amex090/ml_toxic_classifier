0441f66d76c16f6576b29af2e58271657be90716
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-05-29 22:02:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Item(BaseModel):
 
 
 app = FastAPI()
-classifier = pipeline("text-classification")
+classifier = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
 
 
 @app.get("/")
