537a3035648e45d9eb89f5f8709e4e9f3561bc53
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-05-29 21:59:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ class Item(BaseModel):
 
 
 app = FastAPI()
-classifier = pipeline("text-classification")
+classifier = pipeline(
+                      "text-classification", model="cmarkea/distilcamembert-base-sentiment"
+                     )
 
 
 @app.get("/")
